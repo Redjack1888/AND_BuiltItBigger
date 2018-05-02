0ef2290e32fbc194ded740281c94317dcc6beb06
Alessandro Mana <alessandro.mana@gmail.com>
2018-05-02 02:12:59 +0200

    Add Interstitial Ad
    Followed these instructions
    https://developers.google.com/mobile-ads-sdk/docs/admob/android/interstitial
    to add an interstitial ad to the free version. Display the ad after the user hits the button, but before the joke is shown.

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,11 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.example.myandroidlibrary.JokeDisplayActivity;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 
 
 /**
@@ -28,7 +32,8 @@ public class MainActivityFragment extends Fragment {
     public String loadedJoke = null;
 
     public boolean testFlag = false;
-
+    PublisherInterstitialAd mPublisherInterstitialAd = null;
+    String LOG_TAG = "FREEDEBUG";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -37,13 +42,56 @@ public class MainActivityFragment extends Fragment {
 
         AdView mAdView = root.findViewById(R.id.adView);
 
+        //Set up for pre-fetching interstitial ad request
+        mPublisherInterstitialAd = new PublisherInterstitialAd(getContext());
+        mPublisherInterstitialAd.setAdUnitId("/6499/example/interstitial");
+
+        mPublisherInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                //process the joke Request
+                progressBar.setVisibility(View.VISIBLE);
+                tellJoke();
+
+                //pre-fetch the next ad
+                requestNewInterstitial();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+
+                Log.i(LOG_TAG, "onAdFailedToLoad: ad Failed to load. Reloading...");
+
+                //prefetch the next ad
+                requestNewInterstitial();
+
+            }
+
+            @Override
+            public void onAdLoaded() {
+                Log.i(LOG_TAG, "onAdLoaded: interstitial is ready!");
+                super.onAdLoaded();
+            }
+        });
+
+        //Kick off the fetch
+        requestNewInterstitial();
+
         // Set onClickListener for the button
         Button button = root.findViewById(R.id.joke_button);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
-                tellJoke();
+                if (mPublisherInterstitialAd.isLoaded()) {
+                    Log.i(LOG_TAG, "onClick: interstitial was ready");
+                    mPublisherInterstitialAd.show();
+                } else {
+                    Log.i(LOG_TAG, "onClick: interstitial was not ready.");
+                    progressBar.setVisibility(View.VISIBLE);
+                    tellJoke();
+                }
             }
         });
 
@@ -59,6 +107,7 @@ public class MainActivityFragment extends Fragment {
         mAdView.loadAd(adRequest);
         return root;
     }
+
     public void tellJoke(){
         new EndpointsAsyncTask().execute(this);
     }
@@ -73,6 +122,14 @@ public class MainActivityFragment extends Fragment {
             context.startActivity(intent);
             progressBar.setVisibility(View.GONE);
         }
+    }
 
+    private void requestNewInterstitial() {
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+                //.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("EA27D37DF5448BF42AA5F7A6D4F11A9B")
+                .build();
+
+        mPublisherInterstitialAd.loadAd(adRequest);
     }
 }
